0bc9b946f695540a6ece6b2c2d2be6f4193af3ab
hideokawahara-mg <h.kawahara@m-gild.com>
2021-04-08 11:32:29 +0900

    初期段階の作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_media/instagram_media.dart';
-import 'package:share/share.dart';
+// image_picker
+import 'package:image_picker/image_picker.dart';
+
+// image_gallery_saver
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+// esys_flutter_share
+import 'package:esys_flutter_share/esys_flutter_share.dart';
+
+import 'dart:io'; //File
+import 'dart:typed_data'; // Uint8List
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo23',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'esys_flutter_share'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,20 +40,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
+  File _image;
+  final picker = ImagePicker();
 
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
-  var imageUrls = [];
-  var imageCaptions = [];
+  // 画像の読み込み
+  Future _getImage() async {
+    //final pickedFile = await picker.getImage(source: ImageSource.camera);//カメラ
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);//アルバム
+
+    if(pickedFile != null) {
+      setState((){
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  // 画像の保存
+  Future _saveImage() async {
+    if(_image != null) {
+      Uint8List _buffer = await _image.readAsBytes();
+      final result = await ImageGallerySaver.saveImage(_buffer);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,91 +69,41 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          InkWell(
-            onTap: () {
-              // _getIGMedia(context);
-              _share();
-            },
-            child: Container(
-                width: 200,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text('IG PICKER'))),
-          ),
-          StreamBuilder(
-              stream: Stream.value(imageUrls),
-              builder: (context, snapshot) {
-                if (snapshot.data == null) {
-                  return Center(child: Text('Null data'));
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _image == null
+                ? Text('No image selected.')
+                : Image.file(_image),
+            RaisedButton(
+              child: Text('保存'),
+              onPressed: _saveImage,
+            ),
+            RaisedButton(
+              child: Text('テキスト共有'),
+              onPressed: () {
+                Share.text('title', 'Share text.', 'text/plain');
+              },
+            ),
+            RaisedButton(
+              child: Text('画像共有'),
+              onPressed: () async {
+                if(_image != null) {
+                  Uint8List _buffer = await _image.readAsBytes();
+                  await Share.file(
+                      'Share image', 'photo.jpg', _buffer, 'image/jpg');
                 }
-                if ((snapshot.data).length == 0) {
-                  return Center(child: Text('No Media to Display'));
-                } else {
-                  return Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height - 200,
-                      child: GridView.count(
-                          crossAxisCount: 3,
-                          childAspectRatio:
-                              ((MediaQuery.of(context).size.width * 0.22) /
-                                  100),
-                          children: List.generate(imageUrls.length, (index) {
-                            return Column(
-                              children: [
-                                Container(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.33,
-                                  height: 120,
-                                  decoration: BoxDecoration(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(5)),
-                                      color: Colors.grey,
-                                      image: DecorationImage(
-                                          image: NetworkImage(imageUrls[index]),
-                                          fit: BoxFit.cover)),
-                                ),
-                                Text(_determineText(imageCaptions[index]))
-                              ],
-                            );
-                          })));
-                }
-              })
-        ],
+              },
+            ),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getImage,
+        tooltip: 'Pick',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  _getIGMedia(context) async {
-    final result = await Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => InstagramMedia(
-                mediaTypes: 0,
-                appID: '514290619574782',
-                appSecret: '91e61c63db2f2d2026e43ab583534028')));
-    setState(() {
-      imageUrls = result[0];
-      imageCaptions = result[4];
-    });
-    print(result[0]); //urls
-    print(result[1]); //timestamps
-    print(result[2]); //IDs
-    print(result[3]); //types (IMAGE, VIDEO, or CAROUSEL_ALBUM)
-    print(result[4]); //captions
-  }
-
-  Future _share() async {
-    await Share.share("ここに共有したい文字列");
-  }
-
-  _determineText(input) {
-    if (input == null) {
-      return 'No Captions';
-    } else {
-      return input;
-    }
   }
 }
