e0e4ec0d2a402bafe20a08aad7975efa7ddeec12
hideokawahara-mg <h.kawahara@m-gild.com>
2021-04-08 18:16:14 +0900

    feat: add video extension

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:esys_flutter_share/esys_flutter_share.dart';
 
 import 'dart:io'; //File
 import 'dart:typed_data'; // Uint8List
-
+import 'package:share_extend/share_extend.dart';
 
 void main() {
   runApp(MyApp());
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   // 画像の読み込み
   Future _getImage() async {
     //final pickedFile = await picker.getImage(source: ImageSource.camera);//カメラ
-    final pickedFile = await picker.getImage(source: ImageSource.gallery);//アルバム
+    final pickedFile = await picker.getVideo(source: ImageSource.gallery);//アルバム
 
     if(pickedFile != null) {
       setState((){
@@ -90,9 +90,10 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Text('画像共有'),
               onPressed: () async {
                 if(_image != null) {
-                  Uint8List _buffer = await _image.readAsBytes();
-                  await Share.file(
-                      'Share image', 'photo.jpg', _buffer, 'image/jpg');
+                  ShareExtend.share(_image.path, "video");
+                  // Uint8List _buffer = await _image.readAsBytes();
+                  // await Share.file(
+                  //     'Share image', 'photo.jpg', _buffer, 'image/jpg');
                 }
               },
             ),
